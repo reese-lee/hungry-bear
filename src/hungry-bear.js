865f1e0b35d6e634bd1a16b850b4fc0bb73a5e4f
Marc Davies and Reese Lee <marc@>
2019-06-03 10:39:31 -0700

    switching to ES6 code

--- src/hungry-bear.js
+++ src/hungry-bear.js
@@ -3,35 +3,38 @@
 
 // START OF NEW CODE
 
-export let bear = {
-  foodLevel: 10,
-  setHunger: function() {
-    const hungerInterval = setInterval(() => {
-      this.foodLevel--;
-      if (this.didYouGetEaten() == true) {
-        clearInterval(hungerInterval);
-        return "You got eaten!";
-      }
-    }, 1000);
-  },
-  didYouGetEaten: function() {
-    if (this.foodLevel > 0) {
-      return false;
-    } else {
-      return true;
-    }
-  },
-  feed: function(amount) {
-    let that = this;
-    return function(food) {
-      that.foodLevel += amount;
-      return `The bear ate the ${food}! Food level goes up ${amount}!`;
-    }
-  }
-};
-
-// This is supposed to go somewhere but doesn't work:
+// export let bear = {
+//   foodLevel: 10,
+//   setHunger: function() {
+//     const hungerInterval = setInterval(() => {
+//       this.foodLevel--;
+//       if (this.didYouGetEaten() == true) {
+//         clearInterval(hungerInterval);
+//         return "You got eaten!";
+//       }
+//     }, 1000);
+//   },
+//   didYouGetEaten: function() {
+//     if (this.foodLevel > 0) {
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   },
+//   feed: function(amount) {
+//     let that = this;
+//     return function(food) {
+//       that.foodLevel += amount
+//       return `The bear ate the ${food}! Food level goes up ${amount}!`
+//     }
+//   }
+// };
+//
 // bear.eatSmall = bear.feed(5);
+
+// module.exports = {
+//   bear:bear
+// }
 
 //END OF NEW CODE
 
@@ -40,30 +43,30 @@ export let bear = {
 //
 //START OF OLD CODE
 //
-// export class HungryBear {
-//
-//   constructor(name) {
-//     this.name = name;
-//     this.foodLevel = 10;
-//   }
-//
-//   setHunger() {
-//     setInterval(() => {
-//       this.foodLevel--;
-//     }, 1000);
-//   }
-//
-//   didYouGetEaten() {
-//     if (this.foodLevel > 0) {
-//       return false;
-//     } else {
-//       return true;
-//     }
-//   }
-//
-//   feed() {
-//     this.foodLevel = 10;
-//   }
-// }
+export class HungryBear {
+
+  constructor(name) {
+    this.name = name;
+    this.foodLevel = 10;
+  }
+
+  setHunger() {
+    setInterval(() => {
+      this.foodLevel--;
+    }, 1000);
+  }
+
+  didYouGetEaten() {
+    if (this.foodLevel > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  feed() {
+    this.foodLevel = 10;
+  }
+}
 //
 // END OF OLD CODE
