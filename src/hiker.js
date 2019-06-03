957216e7cbd2bac25d97af85247e33a9ed17dff0
Student <example@example.com>
2019-06-03 16:02:49 -0700

    add inventory of foods to variable backpack of Hiker

--- src/hiker.js
+++ src/hiker.js
@@ -5,15 +5,9 @@ export class Hiker {
   constructor(name) {
     this.name = name;
     this.alive = true;
+    this.backpack =
+    {"trailMix": 5 , "granolaBar": 3, "peanutButterSandwich": 3, "bodyParts": 4};
   }
-
-  // areYouDead() {
-  //   if (HungryBear.foodLevel > 0) {
-  //     this.alive = true;
-  //   } else {
-  //     this.alive = false;
-  //   }
-  // }
 
   areYouDead() {
     if (HungryBear.foodLevel === 0) {
