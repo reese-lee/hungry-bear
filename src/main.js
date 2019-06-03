eb3e5f93e65bf1ca250761de2ce41c9b056fba50
Marc Davies <marcdaviesriot@gmail.com>
2019-06-03 00:43:25 -0700

    add backend logic

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { HungryBear } from './hungry-bear.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+$(document).ready(function() {
+  let hungryBear = new HungryBear;
+  return hungryBear;
+});
