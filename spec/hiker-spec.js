00eda9275fee53c9cd69a6bcfd01232d429f34ec
Marc Davies and Reese Lee <reese@>
2019-06-03 10:37:48 -0700

    add hiker-spec.js test file

---
+++ spec/hiker-spec.js
@@ -0,0 +1,18 @@
+import { Hiker } from './../src/hiker.js';
+import { HungryBear } from './../src/hungry-bear.js';
+
+describe('Hiker', function() {
+  let Linda = new Hiker("Linda");
+
+  it('should have a name and an alive status of true', function() {
+    expect(Linda.name).toEqual("Linda");
+    expect(Linda.alive).toEqual(true);
+  });
+
+  it('should be dead if eaten by bear', function() {
+    HungryBear.foodLevel = 0;
+    Linda.areYouDead();
+    expect(Linda.alive).toEqual(false);
+  });
+  
+});
