824938d04bc582ef3fabad3d06c97ef113681733
Student <example@example.com>
2019-06-04 14:57:51 -0700

    add functions for UI to display initial levels, hiker, and bear images

--- src/main.js
+++ src/main.js
@@ -1,15 +1,62 @@
 import { HungryBear } from './hungry-bear.js';
+import { Hiker } from './hiker.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import CuteBear from './img/cutebear.png';
+import Hikers from './img/hikers.png';
 
 let cuteBear = document.getElementById('cuteBear');
 cuteBear.src = CuteBear;
 
+let hikers = document.getElementById('hiker');
+hikers.src = Hikers;
+
 
 $(document).ready(function() {
   let hungryBear = new HungryBear;
-  return hungryBear;
+  // return hungryBear;
+  let hiker = new Hiker;
+  // return hiker;
+  StatTrack(hungryBear);
+  hungryBear.setHunger();
+
+  $("#trailMix").text(hiker.backpack["trailMix"]);
+  $("#trailMixButton").click(function() {
+    $("#trailMix").text(hiker.backpack["trailMix"]);
+    hiker.foodAmount();
+    hungryBear.feedTrailMix(hiker);
+    console.log(hiker.backpack["trailMix"]);
+  });
+
+  $("#granolaBar").text(hiker.backpack["granolaBar"]);
+  $("#granolaBarButton").click(function() {
+    $("#granolaBar").text(hiker.backpack["granolaBar"]);
+    hungryBear.feedGranolaBar(hiker);
+  });
+
+  $("#peanutButterSandwich").text(hiker.backpack["peanutButterSandwich"]);
+  $("#peanutButterSandwichButton").click(function() {
+    $("#peanutButterSandwich").text(hiker.backpack["peanutButterSandwich"]);
+    hungryBear.feedPeanutButterSandwich(hiker);
+  });
+
+  $("#bodyPart").text(hiker.backpack["bodyParts"]);
+  $("#bodyPartButton").click(function() {
+    $("#bodyPart").text(hiker.backpack["bodyParts"]);
+    hungryBear.feedBodyPart(hiker);
+  });
+
+
+  function StatTrack(hungryBear) {
+    setInterval(() => {
+      $("#hunger").text(hungryBear.foodLevel);
+      hiker.areYouDead();
+      if(hungryBear.foodLevel === 0) {
+        $(".row").hide();
+        $(".dead").show();
+      }
+    }, 1000);
+  }
 });
