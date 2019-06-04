e7d9eb59cb81290a8c763dc3e1ed12284a02b918
Student <example@example.com>
2019-06-04 14:55:27 -0700

    add foodAmount function to deactivate buttons

--- src/hiker.js
+++ src/hiker.js
@@ -9,6 +9,14 @@ export class Hiker {
     {"trailMix": 5 , "granolaBar": 3, "peanutButterSandwich": 3, "bodyParts": 4};
   }
 
+  foodAmount() {
+    var trailMixButton = document.getElementById("trailMixButton");
+    trailMixButton.disabled = false;
+    if(this.backpack["trailMix"] === 0) {
+      trailMixButton.disabled = true;
+    }
+  }
+
   areYouDead() {
     if (HungryBear.foodLevel === 0) {
       return this.alive = false;
