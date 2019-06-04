31fe0ccbde0ec86814d7cf8ccb84c6ec9164c0df
Student <example@example.com>
2019-06-04 13:18:18 -0700

    add code for adding in image of bear

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import CuteBear from './img/cutebear.png';
+
+let cuteBear = document.getElementById('cuteBear');
+cuteBear.src = CuteBear;
+
 
 $(document).ready(function() {
   let hungryBear = new HungryBear;
